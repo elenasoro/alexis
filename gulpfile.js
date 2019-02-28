c63ce8f9750292a2eec5013ae1a2ef47f6f03b16
Elena <Elena@ap-sosnovskayaen.local>
2019-02-28 22:26:01 +0300

    add full project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const image = require('gulp-image');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('image', function () {
+    gulp.src('images/*')
+        .pipe(image())
+        .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build/styles/'));
+});
+
+
+gulp.task('fonts', function () {
+    return gulp.src('fonts/*')
+        .pipe(gulp.dest('build/fonts'))
+});
+
+gulp.task('watch', ['sass', 'image', 'fonts'], function () {
+    gulp.watch('styles/*.scss', ['sass']);
+    gulp.watch('images/*', ['image']);
+    gulp.watch('fonts/*', ['fonts']);
+});
+
+gulp.task('default', ['watch', 'fonts', 'image']);
